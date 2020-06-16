f70c67e377c548d6adee486dd580cde031f8ed70
Nicolás Ubaldini <nicolas.ubaldini@davinci.edu.ar>
2020-06-16 05:01:50 -0300

    Primer commit

---
+++ src/main/java/ar/edu/davinci/dvds20201cg13/DvDs20201cG13Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20201cG13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20201cG13Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20201cg13/DvDs20201cG13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20201cG13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
